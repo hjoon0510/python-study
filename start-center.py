8c246869013790c2abd8bd44524225fed3901843
Hyeonjun Lim <hjoon0510@gmail.com>
2017-12-29 23:28:14 +0900

    Add start-center.py
    Signed-off-by: Hyeonjun Lim <hjoon0510@gmail.com>

---
+++ start-center.py
@@ -0,0 +1,13 @@
+a = int(input('입력하세요: '))
+for i in range(a):
+    for j in range(a - 1):
+        if j < i:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    for f in range(a - i):
+
+        print("*",end="")
+
+    print()
+
